b8d283d496c54ca27baebd97e79f527c31a512b0
Katchietsnouva <philipaswa01@gmail.com>
2024-03-14 18:32:45 +0300

    first commit 0.21

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,69 @@
+// import 'package:flutter/material.dart';
+// import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Nouva Graphics ',
+//       theme: ThemeData(
+//         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         colorScheme:
+//             ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 183, 30, 58)),
+//         useMaterial3: true,
+//       ),
+//       // routes:{
+//       //   "/" :(context) => MyHomePage(title: title)
+//       // },
+//       home: const MyHomePage(title: 'Nouva Graphics'),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
+
+
+
+// class MyHomePage extends StatelessWidget {
+//   final String title;
+
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(title),
+//       ),
+//       body: Center(
+//         child: Container(
+//           width: 300,
+//           height: 300,
+//           decoration: BoxDecoration(
+//             color: Colors.white.withOpacity(0.5), // Adjust opacity as needed
+//             borderRadius: BorderRadius.circular(20.0), // Add border radius for rounded corners
+//           ),
+//           padding: EdgeInsets.all(20.0), // Add padding for spacing
+//           child: const ModelViewer(
+//             src: 'assets/nouva_graphics.glb',
+//             autoRotate: true,
+//             rotationPerSecond:  "30deg",
+//             backgroundColor: Colors.transparent,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
 import 'package:flutter/material.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 
@@ -6,58 +72,50 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nouva Graphics ',
+      title: 'Nouva Graphics',
       theme: ThemeData(
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 183, 30, 58)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 183, 30, 58)),
         useMaterial3: true,
       ),
-      // routes:{
-      //   "/" :(context) => MyHomePage(title: title)
-      // },
       home: const MyHomePage(title: 'Nouva Graphics'),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+class MyHomePage extends StatelessWidget {
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        ),
-        body: Center(
-            child: Container(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Container(
           width: 300,
           height: 300,
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.5),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          padding: EdgeInsets.all(20.0),
           child: const ModelViewer(
             src: 'assets/nouva_graphics.glb',
-            // autoRotate: true,
-            backgroundColor: Colors.transparent,
-            // animationDuration: Duration(milliseconds: 1000),
+            autoRotate: true,
+            rotationPerSecond: "30deg", // Adjust rotation speed as needed
           ),
-        )
-            // body: ModelViewer(src: 'assets/ct_sas.glb'),
-
-            ));
+        ),
+      ),
+    );
   }
 }
+
