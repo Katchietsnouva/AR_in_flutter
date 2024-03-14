ac620440cd91c9996ffbd11174706875e81e7a22
Katchietsnouva <philipaswa01@gmail.com>
2024-03-14 15:25:40 +0300

    first commit 0.06

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Nouva Graphics ',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Nouva Graphics'),
     );
   }
 }
