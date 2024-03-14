c8096709b6aa427157299598e05603a8df4cd581
Katchietsnouva <philipaswa01@gmail.com>
2024-03-14 14:51:14 +0300

    first commit 0.01

--- lib/main.dart
+++ lib/main.dart
@@ -49,15 +49,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: ModelViewer(src: 'assets/ct_sas.glb')
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: const ModelViewer(
+          src: 'assets/nouva_graphics.glb',
+          // autoRotate: true,
+          backgroundColor: Colors.transparent,
+          // animationDuration: Duration(milliseconds: 1000),
+          ),
+      ) 
+      // body: ModelViewer(src: 'assets/ct_sas.glb')
     );
 
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    //     title: Text(widget.title),
-    //   ),
     //   body: Center(
     //     child: Column(
     //       mainAxisAlignment: MainAxisAlignment.center,
