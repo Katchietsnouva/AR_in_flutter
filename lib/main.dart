ee71e23469aeac6c2819f6ce10a0307a4171eeb6
Katchietsnouva <philipaswa01@gmail.com>
2024-03-14 15:39:16 +0300

    first commit 0.07

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nouva Graphics ',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Nouva Graphics'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -26,65 +26,31 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
+  
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Container(
-        child: const ModelViewer(
-          src: 'assets/nouva_graphics.glb',
-          // autoRotate: true,
-          backgroundColor: Colors.transparent,
-          // animationDuration: Duration(milliseconds: 1000),
-          
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Container(
+          child: const ModelViewer(
+            src: 'assets/nouva_graphics.glb',
+            // autoRotate: true,
+            backgroundColor: Colors.transparent,
+            // animationDuration: Duration(milliseconds: 1000),
           ),
-      ) 
-      // body: ModelViewer(src: 'assets/ct_sas.glb')
-    );
-
-    //   body: Center(
-    //     child: Column(
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         const Text(
-    //           'You have pushed the button this many times:',
-    //         ),
-    //         Text(
-    //           '$_counter',
-    //           style: Theme.of(context).textTheme.headlineMedium,
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: _incrementCounter,
-    //     tooltip: 'Increment',
-    //     child: const Icon(Icons.add),
-    //   ), // This trailing comma makes auto-formatting nicer for build methods.
-    // );
+        )
+        // body: ModelViewer(src: 'assets/ct_sas.glb')
+        );
   }
 }
